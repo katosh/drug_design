7af508be435517728793094c90fd8e2f9076adb1
Dominik <dominik.otto@gmail.com>
2014-01-26 20:37:33 +0100

    corners of P

--- butan.m
+++ butan.m
@@ -2,8 +2,8 @@ P = importdata('butan50.txt');
 [eigenvectors eigenvalues] = eig(P);
 
 % just 5
-eigve = eigenvectors(:,1:5);
-eigva = eigenvalues(1:5);
+eigve = eigenvectors(:,1:3);
+eigva = eigenvalues(1:3);
 
 ort = orthonormal(eigve, P);
 corners = pccap(P);
