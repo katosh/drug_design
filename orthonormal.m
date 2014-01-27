8f3df75fa4f862040f0e78ad7fdf6e3cc7a615e1
Dominik <dominik.otto@gmail.com>
2014-01-27 23:16:26 +0100

    corrected orthonormalizing

--- orthonormal.m
+++ orthonormal.m
@@ -1,9 +1,11 @@
 function Q = orthonormal(X, P)
     Q(:,1) = X(:,1) / norm(X(:,1));
-    done = zeros(length(Q),1);
     for i=2:length(X(1,:))
-        done = (Q(:,i-1)'*P*X(:,i)) * Q(:,i-1) + done;
-        Q(:,i) = X(:,i) - done;
-        Q(:,i) = Q(:,i) / norm(Q(:,i));
+        otherparts = zeros(length(Q),1);
+        for k=1:i-1
+            otherparts = (Q(:,k)'*P*X(:,i)) * Q(:,k) + otherparts;
+        end
+        Q(:,i) = X(:,i) - otherparts;
+        Q(:,i) = Q(:,i) / sqrt(Q(:,i)'*P*Q(:,i));
     end
 end
